53419b238808c999f4a4290f4a9d5a0958d8f3c6
Mike Purvis <mpurvis@clearpath.ai>
2021-06-03 20:42:41 +0000

    Slight simplification.

--- ros/cpp_common.nix
+++ ros/cpp_common.nix
@@ -15,29 +15,23 @@ stdenv.mkDerivation {
 
     nativeBuildInputs = [ colcon ];
 
-    phases = ["unpackPhase" "patchPhase" "preBuildPhase" "buildPhase" "fixupPhase"];
+    phases = ["unpackPhase" "patchPhase" "buildPhase" "fixupPhase"];
 
     inherit colcon;
     inherit catkin;
 
     cmakeFlags = [ "-DCATKIN_GLOBAL_INCLUDE_DESTINATION=$dev/include" ];
 
-    preBuildPhase = ''
-      export BOOST_THREAD_LIB="boost_thread"
-      export BOOST_SYSTEM_LIB="boost_system"
-      source $catkin/local_setup.sh
-    '';
-
     buildPhase = ''
-      echo "CPP: $CMAKE_PREFIX_PATH"
-      echo "PP: $PYTHONPATH"
-      mkdir -p $dev
-      cd /build
-      $colcon/bin/colcon \
-        build \
-        --paths source \
+      source $catkin/local_setup.sh
+      $colcon/bin/colcon build \
+        --build-base /build \
+        --paths /build/source \
         --merge-install \
         --install-base $out
+    '';
+
+    preFixup = ''
       rm $out/{setup.sh,.catkin,.colcon_install_layout,COLCON_IGNORE}
       moveToOutput "share/*/cmake" "$dev"
     '';
